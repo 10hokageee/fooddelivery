ac90b607bcaf3962c582edc66bba04447533dea7
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-06-30 00:17:25 +0300

    added new section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,55 @@
-$(function(){
 
+  $(function(){
+    $('.restaurants__list-items').slick({
+     infinite: false,
+     slidesToShow: 4,
+     slidesToScroll: 4,
+     prevArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
+     nextArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#arrow-right"></use></svg></button>'
+    });
+   });
+
+
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+		.pipe(dest('app/images')); 
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger'); 
+  burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active'); 
+  });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+ 
+  const burger = document.querySelector('.burger'); 
+  const mobileMenu = document.querySelector('.menu'); 
+  const bodyLock = document.querySelector('body'); 
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('menu--active'); 
+    if (mobileMenu.classList.contains('menu--active'))  { 
+      burger.classList.add('burger--active'); 
+      bodyLock.classList.add('lock'); 
+    }
+    else { 
+      burger.classList.remove('burger--active'); 
+      bodyLock.classList.remove('lock'); 
+    }
+  });
+});
+
